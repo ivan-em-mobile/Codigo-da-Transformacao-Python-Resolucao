003629d6d520e6680ec87799d13843d88185df45
ivan-em-mobile <ivan.paulino@vocacao.org.br>
2025-09-02 09:28:51 -0300

    update_resolucao

--- modulo_01/intr_python_primeiro_programa.py
+++ modulo_01/intr_python_primeiro_programa.py
@@ -1,3 +1,7 @@
+"""
+Atividade de saudação e teste em python.
+"""
+
 # A função input() solicita uma entrada do usuário e armazena o valor na variável 'nome'.
 nome = input("Qual é o seu nome? ")
 
